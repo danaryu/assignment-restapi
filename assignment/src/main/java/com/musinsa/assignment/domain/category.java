9ad32e192bac737dfc769e6c5fb8cf24135fa9cd
danaryu <dana40426@gmail.com>
2021-12-23 20:30:06 +0900

    Lombok Dependency 추가, Datasource(JPA) Setting

---
+++ assignment/src/main/java/com/musinsa/assignment/domain/category.java
@@ -0,0 +1,14 @@
+package com.musinsa.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+
+}
